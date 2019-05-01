004b316f86c6989e8613ebe5c94ae77bd334e758
Go Tae Geon <gtg7784@lab-c.me>
2019-05-01 10:07:05 +0900

    beautifier setting fin;

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,8 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "react/prefer-stateless-function": 0,
+        "react/jsx-filename-extension": 0,
+        "react/jsx-one-expression-per-line": 0,
     }
 };
