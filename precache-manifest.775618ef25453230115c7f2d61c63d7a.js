2eccbb2ee39f845eaab63dbcb46898f1fcdf2db5
Go Tae Geon <gtg7784@lab-c.me>
2019-05-23 04:08:53 +0900

    Updates

---
+++ precache-manifest.775618ef25453230115c7f2d61c63d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d325296426d101ee293becbc3a1721",
+    "url": "sunrinvoca.github.io/index.html"
+  },
+  {
+    "revision": "7805b33dc9968634b231",
+    "url": "sunrinvoca.github.io/static/css/main.042376f5.chunk.css"
+  },
+  {
+    "revision": "38a2ebeab0a7d58caf16",
+    "url": "sunrinvoca.github.io/static/js/2.df0da3e1.chunk.js"
+  },
+  {
+    "revision": "7805b33dc9968634b231",
+    "url": "sunrinvoca.github.io/static/js/main.a02ba825.chunk.js"
+  },
+  {
+    "revision": "2b01dba29656fc0afec5",
+    "url": "sunrinvoca.github.io/static/js/runtime~main.28b26d6a.js"
+  },
+  {
+    "revision": "2343bab13318e49e6e04d1a9dafa8df6",
+    "url": "sunrinvoca.github.io/static/media/back.2343bab1.jpeg"
+  }
+]);
