f68e0ddc923a85fd1429acf2070320ced93cf3bb
Go Tae Geon <gtg7784@lab-c.me>
2019-05-24 01:37:30 +0900

    ADD Wrong

---
+++ src/containers/Wrong/Wrong.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Wrong.scss';
+
+class Wrong extends Component {
+  render() {
+    return (
+      <div className="Wrong">
+            Wrong
+      </div>
+    );
+  }
+}
+
+export default Wrong;
