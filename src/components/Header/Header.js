6e567842cb73c915fa9fbbaedbd587ea374f5ae3
Go Tae Geon <gtg7784@lab-c.me>
2019-05-24 01:37:14 +0900

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,10 +26,9 @@ export default class Header extends Component {
           <Link to="/">Sunrin Voca</Link>
         </h2>
         <ul>
-          <Link to="/">Main</Link>
           <Link to="/voca">Voca</Link>
           <Link to="/exam">Exam</Link>
-          {/* <Link to="/login">Login</Link> */}
+          <Link to="/wrong">Wrong</Link>
         </ul>
       </nav>
     );
