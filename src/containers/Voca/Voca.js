9b5bcc3c658409288b3fc7813db497513bd7ea6f
Go Tae Geon <gtg7784@lab-c.me>
2019-05-24 19:48:34 +0900

    update voca

--- src/containers/Voca/Voca.js
+++ src/containers/Voca/Voca.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import './Voca.scss';
 
 import words from 'assets/db/words.json';
 
 class Voca extends Component {
+
   path = (items) => {
     if(window.location.pathname === '/voca'){
       return`/voca/${items.day}`;
@@ -14,25 +15,33 @@ class Voca extends Component {
       return `/wrong/${items.day}`
     }
   }
+
   render() {
+    const { type } = this.props.match.params;
+
     return (
       <div className="Voca">
-        <ul>
-          {words.map((items, i) => (
-            <li key={i}>
-              <span>
-                <h3>
-                  <Link to={this.path(items)}>DAY - {items.day}
-                  </Link>
-                </h3>
-                <div>{items.description}</div>
-              </span>
-            </li>
-          ))}
-        </ul>
+        <h1>{type}</h1>
+
+        <div>
+          <ul>
+            {words.map((items, i) => (
+              <li key={i}>
+                <span>
+                  <h3>
+                    <Link to={this.path(items)}>
+                      DAY - {items.day}
+                    </Link>
+                  </h3>
+                  <div>{items.description}</div>
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
-export default Voca;
+export default withRouter(Voca);
