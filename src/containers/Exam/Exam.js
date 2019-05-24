fd9e450d41609a0d70bc6bbcdeefb65a338b5ba9
Go Tae Geon <gtg7784@lab-c.me>
2019-05-24 14:17:14 +0900

    update exam

--- src/containers/Exam/Exam.js
+++ src/containers/Exam/Exam.js
@@ -1,26 +1,98 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import './Exam.scss';
 
 import exams from 'assets/db/exams.json';
 
 class Exam extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setState({
+      number: 0,
+    });
+  }
+
   render() {
+    const { id } = this.props.match.params;
+
     return (
       <div className="Exam">
-        <ul>
-          {exams.map((items, i) => (
-            <li key={i}>
-              <span>
-                <div>{items.enko}</div>
-                <div>{items.underline}</div>
-              </span>
+        <h1>DAY - {id}</h1>
+
+        <div>
+          <ul>
+            {exams[id - 1].enko.map((items, i) => (
+              <li id="enko" key={i}>
+                { `${i + 1}. ${items}` }
+                <input type="text" />
+              </li>
+            ))}
+            {exams[id - 1].underlineSame.map((items, i) => (
+              <li key={i}>
+                {`${i + 21}.  다음 밑줄친 부분과 뜻이 가장 가까운 것을 고르세요.`}
+                <div>
+                  <div>
+                    {`${items[0]} `}
+                    <span>{`${items[1]}`}</span>
+                    {` ${items[2]}`}
+                  </div>
+                  <div>
+                    <input type="checkbox" /> {items[3]}
+                    <input type="checkbox" /> {items[4]}
+                    <input type="checkbox" /> {items[5]}
+                    <input type="checkbox" /> {items[6]}
+                    <input type="checkbox" /> {items[7]}
+                  </div>
+                </div>
+              </li>
+            ))}
+            {exams[id - 1].underlineOpposite.map((items, i) => (
+              <li key={i}>
+                {`${i + 23}.  다음 밑줄친 부분의 반대말로 가장 알맞는 것을 고르세요.`}
+                <div>
+                  <div>
+                    {`${items[0]} `}
+                    <span>{`${items[1]}`}</span>
+                    {` ${items[2]}`}
+                  </div>
+                  <div>
+                    <input type="checkbox" /> {items[3]}
+                    <input type="checkbox" /> {items[4]}
+                    <input type="checkbox" /> {items[5]}
+                    <input type="checkbox" /> {items[6]}
+                    <input type="checkbox" /> {items[7]}
+                  </div>
+                </div>
+              </li>
+            ))}
+            <li id="example">
+              다음 빈칸에 들어갈 가장 알맞는 것을 고르세요.
+              <div>
+                <div>
+                  {exams[id - 1].blankExample.map(items => (
+                    <>{`${items}  `}</>
+                  ))}
+                </div>
+              </div>
             </li>
-          ))}
-        </ul>
+            {exams[id - 1].blank.map((items, i) => (
+              <li key={i} id="blank">
+                <div>
+                  <div>
+                    {`${i + 25}. `}
+                    {`${items[0]} `}
+                    <input type="text" />
+                    {` ${items[1]}`}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
-export default Exam;
+export default withRouter(Exam);
