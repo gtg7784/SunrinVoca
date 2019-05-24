ad5fbc35799683ce8a9c37d0b3cdcf85ede1b4e1
Go Tae Geon <gtg7784@lab-c.me>
2019-05-24 19:48:56 +0900

    update exam

--- src/containers/Exam/Exam.js
+++ src/containers/Exam/Exam.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
 import './Exam.scss';
 
 import exams from 'assets/db/exams.json';
 
 class Exam extends Component {
-  constructor(props) {
-    super(props);
-
-    this.setState({
-      number: 0,
-    });
+  state = {
+    number: 0
   }
 
   render() {
     const { id } = this.props.match.params;
-
     return (
       <div className="Exam">
         <h1>DAY - {id}</h1>
@@ -24,13 +19,13 @@ class Exam extends Component {
           <ul>
             {exams[id - 1].enko.map((items, i) => (
               <li id="enko" key={i}>
-                { `${i + 1}. ${items}` }
+                {`${i + 1}. ${items}`}
                 <input type="text" />
               </li>
             ))}
             {exams[id - 1].underlineSame.map((items, i) => (
               <li key={i}>
-                {`${i + 21}.  다음 밑줄친 부분과 뜻이 가장 가까운 것을 고르세요.`}
+                {`${i +  exams[id - 1].enko.length + 1}.  다음 밑줄친 부분과 뜻이 가장 가까운 것을 고르세요.`}
                 <div>
                   <div>
                     {`${items[0]} `}
@@ -49,7 +44,7 @@ class Exam extends Component {
             ))}
             {exams[id - 1].underlineOpposite.map((items, i) => (
               <li key={i}>
-                {`${i + 23}.  다음 밑줄친 부분의 반대말로 가장 알맞는 것을 고르세요.`}
+                {`${i + exams[id - 1].enko.length + 3}.  다음 밑줄친 부분의 반대말로 가장 알맞는 것을 고르세요.`}
                 <div>
                   <div>
                     {`${items[0]} `}
@@ -67,11 +62,11 @@ class Exam extends Component {
               </li>
             ))}
             <li id="example">
-              다음 빈칸에 들어갈 가장 알맞는 것을 고르세요.
+              {`[${exams[id - 1].enko.length + 5}~${exams[id - 1].enko.length + 7}] 다음 빈칸에 들어갈 가장 알맞는 것을 고르세요.`}
               <div>
                 <div>
-                  {exams[id - 1].blankExample.map(items => (
-                    <>{`${items}  `}</>
+                  {exams[id - 1].blankExample.map((items, i) => (
+                    <Fragment key={i}>{`${items}  `}</Fragment>
                   ))}
                 </div>
               </div>
@@ -80,7 +75,7 @@ class Exam extends Component {
               <li key={i} id="blank">
                 <div>
                   <div>
-                    {`${i + 25}. `}
+                    {`${i +  exams[id - 1].enko.length + 5}. `}
                     {`${items[0]} `}
                     <input type="text" />
                     {` ${items[1]}`}
